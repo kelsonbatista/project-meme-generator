d27d21e7cd2dfd9265a628530c4078e2c855978d
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-11-01 19:24:28 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+// Ref Css https://stackoverflow.com/questions/8935575/align-div-in-the-bottom-of-another-div-using-css/8935607
+// Ref Input File https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file
+// Ref fit image style https://stackoverflow.com/questions/3029422/how-do-i-auto-resize-an-image-to-fit-a-div-container
+
 // insert text
 const inputText = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 
 inputText.addEventListener('input', () => {
   memeText.innerHTML = inputText.value;
+});
+
+// insert image
+const inputFile = document.querySelector('#meme-insert');
+const theImage = document.querySelector('#meme-image');
+
+inputFile.addEventListener('change', () => {
+  theImage.src = URL.createObjectURL(inputFile.files[0]);
 });
